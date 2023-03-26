55707a403e1771207eb8c8fde5b91827f07a979b
owen <osn53387@uga.edu>
2023-03-26 15:11:54 -0400

    bst work

---
+++ BinarySearchTree.java
@@ -0,0 +1,17 @@
+
+
+public class BinarySearchTree {
+    NodeType<T> root;
+
+    public BinarySearchTree() {
+        root.info = null;  
+    }
+
+    public void insert(T key) {
+        if (key.info == root.info) {
+            return;
+        } else {
+            
+        }
+    }
+}
