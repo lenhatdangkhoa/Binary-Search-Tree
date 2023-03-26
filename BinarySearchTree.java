011cf8dba2e472969b76e0cfad36d46310f4b79c
owen <osn53387@uga.edu>
2023-03-26 15:30:24 -0400

    bst insert implemented

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -8,10 +8,28 @@ public class BinarySearchTree {
     }
 
     public void insert(T key) {
-        if (key.info == root.info) {
+        NodeType<T> placeholder = new NodeType<>(root);
+        if (root.info == null) { // adding for empty tree
+            root.info == key.info;
+        } else if (key.compareTo(root) == 0) { // checking for dupe on root node
             return;
         } else {
-            
+            while (key.compareTo(placeholder.info.left) || key.compareTo(placeholder.info.right) == (1 || -1) {
+                if (key.compareTo(placeholder.info) == 0) {
+                    return;
+                }
+                if (key.compareTo(placeholder.info.left) == -1 || placeholder.left == null) {
+                    if (placeholder.left == null) {
+                        placeholder.left.info = key;
+                    } // if
+                    placeholder = placeholder.left;
+                } else if (key.compareTo(placeholder.info.right) == 1 || placeholder.right == null) {
+                    if (placeholder.right == null) {
+                        placeholder.right,info = key;
+                    }
+                    placeholder = placeholder.right;
+                }
+            }
         }
     }
 }
